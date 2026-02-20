cef39e5e765c2a844f13bd443900b887c00ff73b
TomasRojoBiran <tomas.rojo.biran@gmail.com>
2026-02-20 13:16:44 -0300

    day04 test ready

--- 2025/rust/day04/src/part1.rs
+++ 2025/rust/day04/src/part1.rs
@@ -2,3 +2,28 @@ pub fn solve(parsed: Vec<Vec<&str>>) -> u64 {
     println!("{:?}", parsed);
     todo!("Implement solve")
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::parse::parse;
+
+    #[test]
+    fn solves_sample_input() -> Result<(), Box<dyn std::error::Error>> {
+        let input = "..@@.@@@@.
+@@@.@.@.@@
+@@@@@.@.@@
+@.@@@@..@.
+@@.@@@@.@@
+.@@@@@@@.@
+.@.@.@.@@@
+@.@@@.@@@@
+.@@@@@@@@.
+@.@.@@@.@.";
+        let parsed = parse(input)?;
+        let result = solve(parsed);
+
+        assert_eq!(result, 13);
+        Ok(())
+    }
+}
