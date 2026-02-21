f2165cf520a56dddf62b7ad4340ddb9ba2bed58f
TomasRojoBiran <tomas.rojo.biran@gmail.com>
2026-02-21 13:03:32 -0300

    way nicer solution for part 1

--- 2025/rust/day04/src/part1.rs
+++ 2025/rust/day04/src/part1.rs
@@ -1,63 +1,25 @@
+const NEIGHBORS: [(i32, i32); 8] = [
+    (-1, 0),
+    (1, 0),
+    (0, -1),
+    (0, 1),
+    (-1, -1),
+    (-1, 1),
+    (1, -1),
+    (1, 1)
+];
+
 pub fn solve(parsed: Vec<Vec<char>>) -> u64 {
     let mut count = 0;
     let mut count_matrix = vec![vec![0; parsed[0].len()]; parsed.len()];
     for i in 0..parsed.len() {
         for j in 0..parsed[i].len() {
             if parsed[i][j] == '@' {
-                if let Some(si) = i.checked_sub(1) {
-                    if let Some(sj) = j.checked_sub(1) {
-                        if parsed[si][sj] == '@' {
-                            count_matrix[i][j] += 1
-                        }
-                    }
-                }
-                if let Some(ai) = i.checked_add(1) {
-                    if let Some(aj) = j.checked_add(1) {
-                        if ai < parsed.len() && aj < parsed[ai].len() {
-                            if parsed[ai][aj] == '@' {
-                                count_matrix[i][j] += 1
-                            }
-                        }
-                    }
-                }
-                if let Some(ai) = i.checked_add(1) {
-                    if let Some(sj) = j.checked_sub(1) {
-                        if ai < parsed.len() {
-                            if parsed[ai][sj] == '@' {
-                                count_matrix[i][j] += 1
-                            }
-                        }
-                    }
-                }
-                if let Some(si) = i.checked_sub(1) {
-                    if let Some(aj) = j.checked_add(1) {
-                        if aj < parsed[si].len() {
-                            if parsed[si][aj] == '@' {
-                                count_matrix[i][j] += 1
-                            }
-                        }
-                    }
-                }
-                if let Some(si) = i.checked_sub(1) {
-                    if parsed[si][j] == '@' {
-                        count_matrix[i][j] += 1
-                    }
-                }
-                if let Some(ai) = i.checked_add(1) {
-                    if ai < parsed.len() {
-                        if parsed[ai][j] == '@' {
-                            count_matrix[i][j] += 1
-                        }
-                    }
-                }
-                if let Some(sj) = j.checked_sub(1) {
-                    if parsed[i][sj] == '@' {
-                        count_matrix[i][j] += 1
-                    }
-                }
-                if let Some(aj) = j.checked_add(1) {
-                    if aj < parsed[i].len() {
-                        if parsed[i][aj] == '@' {
+                for (di, dj) in NEIGHBORS {
+                    let ni = i as i32 + di;
+                    let nj = j as i32 + dj;
+                    if (0..parsed.len() as i32).contains(&ni) && (0..parsed[ni as usize].len()).contains(&(nj as usize)) {
+                        if parsed[ni as usize][nj as usize] == '@' {
                             count_matrix[i][j] += 1
                         }
                     }
